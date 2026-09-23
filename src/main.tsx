d2b1923339066965fa5ea41beec0fa904c2c51b8
ariefupgriss3-eng <ariefupgriss3@gmail.com>
2026-09-23 21:42:25 +0700

    Load GuruLes location filter styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './dashboard-tabs.css';
+import './location-filter.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
